f6e9ccb8e718d1ac6d826f7a4777ca10d6db0ced
Nic Bolton <bolton21@uwindsor.ca>
2023-10-21 23:50:41 -0400

    add card class'

---
+++ src/Card.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+
+enum class Face
+{
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+};
+
+enum class Suit
+{
+    SPADES,
+    CLUBS,
+    HEARTS,
+    DIAMONDS
+};
+
+class Card
+{
+private:
+    Face _face;
+    Suit _suit;
+
+public:
+    Card() = delete;
+    Card(Face face, Suit suit) : _face(face), _suit(suit){};
+
+    Face face() const { return _face; }
+    Suit suit() const { return _suit; }
+
+    int value() const
+    {
+        switch (_face)
+        {
+        case Face::ACE:
+            return 1;
+        case Face::TWO:
+            return 2;
+        case Face::THREE:
+            return 3;
+        case Face::FOUR:
+            return 4;
+        case Face::FIVE:
+            return 5;
+        case Face::SIX:
+            return 6;
+        case Face::SEVEN:
+            return 7;
+        case Face::EIGHT:
+            return 8;
+        case Face::NINE:
+            return 9;
+        case Face::TEN:
+        case Face::JACK:
+        case Face::QUEEN:
+        case Face::KING:
+            return 10;
+        }
+    }
+
+    bool equals(const Card &other)
+    {
+        return this->value() == other.value();
+    }
+};
+
+bool operator==(const Card &lhs, const Card &rhs)
+{
+    return lhs.suit() == rhs.suit() && lhs.face() == rhs.face();
+}
+std::ostream &operator<<(std::ostream &os, const Card &card)
+{
+    switch (card.face())
+    {
+    case Face::ACE:
+        os.put('A');
+        break;
+    case Face::TWO:
+        os.put('2');
+        break;
+    case Face::THREE:
+        os.put('3');
+        break;
+    case Face::FOUR:
+        os.put('4');
+        break;
+    case Face::FIVE:
+        os.put('5');
+        break;
+    case Face::SIX:
+        os.put('6');
+        break;
+    case Face::SEVEN:
+        os.put('7');
+        break;
+    case Face::EIGHT:
+        os.put('8');
+        break;
+    case Face::NINE:
+        os.put('9');
+        break;
+    case Face::TEN:
+        os.put('1');
+        os.put('0');
+        break;
+    case Face::JACK:
+        os.put('J');
+        break;
+    case Face::QUEEN:
+        os.put('Q');
+        break;
+    case Face::KING:
+        os.put('K');
+        break;
+    default:
+        break;
+    }
+
+    switch (card.suit())
+    {
+    case Suit::SPADES:
+        os.put('S');
+        break;
+    case Suit::CLUBS:
+        os.put('C');
+        break;
+    case Suit::HEARTS:
+        os.put('H');
+        break;
+    case Suit::DIAMONDS:
+        os.put('D');
+        break;
+    default:
+        break;
+    }
+
+    return os;
+}
